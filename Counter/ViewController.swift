d6e7fbc96418bc9569443aad97531b2afd10ee99
rssxzp <rssxzp@icloud.com>
2025-03-12 14:35:13 +0500

    need date and time

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,18 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var resetButton: UIButton!
+    
+    @IBOutlet private weak var historyTextView: UITextView!
     @IBOutlet weak var buttonMinus: UIButton!
     @IBOutlet weak var buttonPlus: UIButton!
+    @IBOutlet weak var resetCountButton: UIButton!
     @IBOutlet weak var countView: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     private var count: Int = 0
-    @IBAction func countPlusOne(_ sender: Any) {
-        count += 1
+    @IBAction func countPlusOne(_ sender: Any) {count += 1
         countView.text = String(count)
+        historyTextView.text += "\n Значение увеличено на 1"
+    }
+    
+    @IBAction func countMinusOne(_ sender: Any) {count -= 1
+        if count < 0 {
+            historyTextView.text += "\n Попытка уменьшить значение счётчика ниже 0"
+            count = 0
+        } else {
+            historyTextView.text += "\n Значение уменьшено на 1"
+            countView.text = String(count)
+        }
+    }
+   
+    @IBAction func resetCount(_ sender: Any) {count = 0
+        countView.text = String(count)
+        historyTextView.text += "\n Значение сброшено"
     }
     
 }
