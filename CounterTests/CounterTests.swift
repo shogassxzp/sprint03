7330def9699e90ab5b3e24ed3eb7d86186464846
rssxzp <rssxzp@icloud.com>
2025-03-12 09:59:11 +0500

    Create a project

---
+++ CounterTests/CounterTests.swift
@@ -0,0 +1,17 @@
+//
+//  CounterTests.swift
+//  CounterTests
+//
+//  Created by Игнат Рогачевич on 12.03.25.
+//
+
+import Testing
+@testable import Counter
+
+struct CounterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
