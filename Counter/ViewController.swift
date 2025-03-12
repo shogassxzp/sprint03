08ad04342bd3f97510cccdcf0f49f69da4836c26
rssxzp <rssxzp@icloud.com>
2025-03-12 10:35:20 +0500

    Counter(easy)

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonPlus: UIButton!
+    @IBOutlet weak var countView: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    private var count: Int = 0
+    @IBAction func countPlusOne(_ sender: Any) {
+        count += 1
+        countView.text = String(count)
+    }
+    
 }
 
